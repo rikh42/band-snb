4a8a65c1ae21c89d9b49f0d3a3322aa7ec21890c
Rik Heywood <rik@smallneatbox.co.uk>
2013-03-10 19:53:46 +0000

    Added some comments to teh Email Interface so that auto suggest works better in IDEs

--- email/EmailInterface.php
+++ email/EmailInterface.php
@@ -10,16 +10,75 @@ namespace snb\email;
 
 interface EmailInterface
 {
+    /**
+     * @param $subject
+     * @return EmailInterface
+     */
     public function subject($subject);
+
+    /**
+     * @param $email
+     * @param null $name
+     * @return EmailInterface
+     */
     public function to($email, $name=null);
+
+    /**
+     * @param $email
+     * @param null $name
+     * @return EmailInterface
+     */
     public function cc($email, $name=null);
+
+    /**
+     * @param $email
+     * @param null $name
+     * @return EmailInterface
+     */
     public function bcc($email, $name=null);
+
+    /**
+     * @param $email
+     * @param null $name
+     * @return EmailInterface
+     */
     public function from($email, $name=null);
+
+    /**
+     * @param $email
+     * @param null $name
+     * @return EmailInterface
+     */
     public function replyTo($email, $name = null);
+
+    /**
+     * @param $html
+     * @return EmailInterface
+     */
     public function htmlBody($html);
+
+    /**
+     * @param $plain
+     * @return EmailInterface
+     */
     public function textBody($plain);
+
+    /**
+     * @param $filename
+     * @param string $mime
+     * @return EmailInterface
+     */
     public function attach($filename, $mime='application/octet-stream');
+
+    /**
+     * @param $tag
+     * @return EmailInterface
+     */
     public function tag($tag);
+
+    /**
+     * @return bool
+     */
     public function send();
 
     public function getHtmlBody();
